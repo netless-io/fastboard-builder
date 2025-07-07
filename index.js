c798a09ea9ca3ade4633fb07a51ef35368a4978f
hqer927 <285569480@qq.com>
2025-07-07 17:04:54 +0800

    update: update slide app only

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import { register } from '@netless/fastboard/full';
-import SlideApp, { addHooks } from "@netless/app-slide";
+// import { register } from '@netless/fastboard/full';
+// import SlideApp, { addHooks } from "@netless/app-slide";
 export * from '@netless/fastboard/full';
 
 // 按需求集成特殊版本的 SlideApp
-register({
-    kind: "Slide",
-    appOptions: { debug: false },
-    src: SlideApp,
-    addHooks,
-});
+// register({
+//     kind: "Slide",
+//     appOptions: { debug: false },
+//     src: SlideApp,
+//     addHooks,
+// });
